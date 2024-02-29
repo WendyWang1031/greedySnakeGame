b889f036479cfedf0c4e41e69e7791e79a5ee960
PinYi Lee <pinyi_lee@icloud.com>
2024-02-29 18:21:56 +0800

    紀錄分數與紀錄最高分數

--- app.js
+++ app.js
@@ -8,26 +8,28 @@ const column = canvas.width / unit; // 320/20=16
 
 let snake = []; //array的每個元素都是一個物件
 //物件的工作是：儲存身體的x,y座標
+function createSnake() {
+  snake[0] = {
+    x: 80,
+    y: 0,
+  };
 
-snake[0] = {
-  x: 80,
-  y: 0,
-};
+  snake[1] = {
+    x: 60,
+    y: 0,
+  };
 
-snake[1] = {
-  x: 60,
-  y: 0,
-};
+  snake[2] = {
+    x: 40,
+    y: 0,
+  };
 
-snake[2] = {
-  x: 40,
-  y: 0,
-};
+  snake[3] = {
+    x: 20,
+    y: 0,
+  };
+}
 
-snake[3] = {
-  x: 20,
-  y: 0,
-};
 class Fruit {
   constructor() {
     this.x = Math.floor(Math.random() * column) * unit;
@@ -64,6 +66,8 @@ class Fruit {
   }
 }
 
+//初始設定
+createSnake();
 let myFruit = new Fruit();
 
 window.addEventListener("keydown", changeDirection);
@@ -78,9 +82,29 @@ function changeDirection(e) {
   } else if (e.key == "ArrowUp" && d != "Down") {
     d = "Up";
   }
+
+  //每次按下上下左右鍵之後，在下一幀被畫出來之前，
+  //不接受任何keydown事件
+  //這樣可以防止連續按鍵導致蛇在邏輯上自殺
+  window.removeEventListener("keydown", changeDirection);
 }
+let highestScore;
+loadHighestScore();
+let score = 0;
+
+document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
 
 function draw() {
+  //每次畫圖之前，確認蛇有沒有咬到自己
+  for (let i = 1; i < snake.length; i++) {
+    if (snake[i].x == snake[0].x && snake[i].y == snake[0].y) {
+      clearInterval(myGame);
+      alert("Game Over");
+      return;
+    }
+  }
+
   //背景全設定為黑色
   ctx.fillStyle = "black";
   ctx.fillRect(0, 0, canvas.width, canvas.height);
@@ -137,11 +161,31 @@ function draw() {
     myFruit.pickALocation();
 
     //更新分數
+    score++;
+    setHighestScore(score);
+    document.getElementById("myScore").innerHTML = "遊戲分數：" + score;
+    document.getElementById("myScore2").innerHTML = "最高分數：" + highestScore;
   } else {
     snake.pop();
   }
 
   snake.unshift(newHead);
+  window.addEventListener("keydown", changeDirection);
 }
 
 let myGame = setInterval(draw, 200);
+
+function loadHighestScore() {
+  if (localStorage.getItem("highestScore") == null) {
+    highestScore = 0;
+  } else {
+    highestScore = Number(localStorage.getItem("highestScore"));
+  }
+}
+
+function setHighestScore(score) {
+  if (score > highestScore) {
+    localStorage.setItem("highestScore", score);
+    highestScore = score;
+  }
+}
